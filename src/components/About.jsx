688056e9aaa7911d6934893e3eb54fffff31d8eb
Oscar Montoya <98506040+oscark22@users.noreply.github.com>
2023-07-21 20:19:49 -0700

    tech section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { Tilt } from "react-tilt";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt="title" className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae ut
+        eum cupiditate nemo a blanditiis, voluptas doloremque, qui animi, quam
+        sunt quasi molestiae dolorem repudiandae consequatur asperiores facilis
+        exercitationem consectetur. Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Nam repellat odit adipisci est, laudantium
+        voluptatibus numquam cupiditate perspiciatis non nostrum accusantium
+        earum, debitis voluptatem! Tempora sed corporis quo dolore explicabo.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore,
+        adipisci impedit. Repudiandae labore non libero dicta itaque autem
+        dolorum voluptates, mollitia delectus, quasi tenetur accusamus omnis
+        natus quidem placeat corporis! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
